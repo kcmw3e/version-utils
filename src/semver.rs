d6b365bdb59b0020ea1a6399d96b89a13cad0294
Casey Walker <kcmw3e@gmail.com>
2025-10-25 00:14:30 -0400

    Add `parse_number`

--- src/semver.rs
+++ src/semver.rs
@@ -2,6 +2,9 @@
 // information.
 //------------------------------------------------------------------------------
 
+use log;
+use std::str::FromStr;
+
 #[derive(Debug)]
 pub struct Version {
     pub major: u64,
@@ -9,3 +12,20 @@ pub struct Version {
     pub patch: u64,
 }
 
+/// Parse a number from a string.
+///
+/// This is just a wrapper around the builtin `parse` method of `str` except it
+/// adds some logging and error information.
+fn parse_number<T>(string: &str) -> Result<T, ParseError>
+where
+    T: FromStr,
+{
+    log::trace!("Parsing number from {string:?}.");
+
+    let Ok(number) = string.parse::<T>() else {
+        log::error!("Could not parse number from {string:?}.");
+        return Err(ParseError::InvalidNumber);
+    };
+
+    Ok(number)
+}
