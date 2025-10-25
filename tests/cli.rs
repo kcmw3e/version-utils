33b52adde1698112d43d529f0f453a2ebc9bdcc9
Casey Walker <kcmw3e@gmail.com>
2025-10-25 11:22:52 -0400

    Add tests for well-formatted input versions

---
+++ tests/cli.rs
@@ -0,0 +1,81 @@
+use assert_cmd::Command;
+
+/// Test bumping well-formatted major numbers.
+#[test]
+fn test_bump_major_good_input() -> Result<(), Box<dyn std::error::Error>> {
+    let tests = [
+        ("1.2.3", "2.0.0"),
+        ("2.0.0", "3.0.0"),
+        ("0.2.3", "1.0.0"),
+        ("5.0.5", "6.0.0"),
+        ("1000.0.0", "1001.0.0"),
+        ("16.1000.0", "17.0.0"),
+        ("9999999999999999999.0.0", "10000000000000000000.0.0"),
+        ("18446744073709551614.0.0", "18446744073709551615.0.0"),
+    ];
+
+    for test in tests {
+        let (input, output) = test;
+
+        Command::cargo_bin("vup")?
+            .arg("major")
+            .write_stdin(input)
+            .assert()
+            .success()
+            .stdout(output);
+    }
+
+    Ok(())
+}
+
+/// Test bumping well-formatted minor numbers.
+#[test]
+fn test_bump_minor_good_input() -> Result<(), Box<dyn std::error::Error>> {
+    let tests = [
+        ("1.2.3", "1.3.0"),
+        ("2.0.0", "2.1.0"),
+        ("0.2.3", "0.3.0"),
+        ("5.0.5", "5.1.0"),
+        ("1000.0.0", "1000.1.0"),
+        ("1.18446744073709551614.18", "1.18446744073709551615.0"),
+    ];
+
+    for test in tests {
+        let (input, output) = test;
+
+        Command::cargo_bin("vup")?
+            .arg("minor")
+            .write_stdin(input)
+            .assert()
+            .success()
+            .stdout(output);
+    }
+
+    Ok(())
+}
+
+/// Test bumping well-formatted patch numbers.
+#[test]
+fn test_bump_patch_good_input() -> Result<(), Box<dyn std::error::Error>> {
+    let tests = [
+        ("1.2.3", "1.2.4"),
+        ("2.0.0", "2.0.1"),
+        ("0.2.3", "0.2.4"),
+        ("5.0.5", "5.0.6"),
+        ("1000.0.0", "1000.0.1"),
+        ("1.1.18446744073709551614", "1.1.18446744073709551615"),
+    ];
+
+    for test in tests {
+        let (input, output) = test;
+
+        Command::cargo_bin("vup")?
+            .arg("patch")
+            .write_stdin(input)
+            .assert()
+            .success()
+            .stdout(output);
+    }
+
+    Ok(())
+}
